5fb4389a1ecb619d5771c3a746aad24541b593d4
svend.peder.oseth <svenno1993@hotmail.com>
2019-02-25 13:07:41 +0100

    Refactored elements according to pageObject.

--- src/test/java/pages/SavingsRequestPage.java
+++ src/test/java/pages/SavingsRequestPage.java
@@ -3,38 +3,60 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 public class SavingsRequestPage {
+    @FindBy(id = "oneTimeInvestmentInput")
+    private WebElement oneTimeInvestmentInput;
+
+    @FindBy(id = "yearsInput")
+    private WebElement yearsInput;
+
+    @FindBy(css = "div.result p")
+    private WebElement actualTotalIncome;
+
+    @FindBy(xpath = "//div[contains(@class,'result')]/div[2]/p")
+    private  WebElement actualNetIncome;
+
+    @FindBy(css = "h1")
+    private WebElement title;
+
+    @FindBy(id = "fundSelect")
+    private WebElement fund;
+
     private WebDriver driver;
 
     public SavingsRequestPage(WebDriver driver){
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public String getTitle (){
-        return driver.findElement(By.cssSelector("h1")).getText();
+        return title.getText();
     }
 
     public void selectFund(String fundName) {
-        new Select(driver.findElement(By.id("fundSelect"))).selectByVisibleText(fundName);
+        new Select(fund).selectByVisibleText(fundName);
     }
 
     public void inputInvestmentPeriod(String years) {
-        driver.findElement(By.id("yearsInput")).sendKeys(years);
-        driver.findElement(By.id("yearsInput")).sendKeys(Keys.TAB);
+        yearsInput.sendKeys(years);
+        yearsInput.sendKeys(Keys.TAB);
     }
 
     public void inputInvestmentAmount(String investment) {
-        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(investment);
-        driver.findElement(By.id("oneTimeInvestmentInput")).sendKeys(Keys.TAB);
+        oneTimeInvestmentInput.sendKeys(investment);
+        oneTimeInvestmentInput.sendKeys(Keys.TAB);
     }
 
     public String getActualTotalIncome(){
-        return driver.findElement(By.cssSelector("div.result p")).getText();
+        return actualTotalIncome.getText();
     }
 
     public String getActualNetIncome(){
-        return driver.findElement(By.xpath("//div[contains(@class,'result')]/div[2]/p")).getText();
+        return actualNetIncome.getText();
     }
 }
